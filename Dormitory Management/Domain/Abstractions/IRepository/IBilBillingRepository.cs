5e167f9781d9000447b26f2facf668fa7d2fa536
Slyrx <hieutrinh09112002@gmail.com>
2024-10-05 10:19:53 +0700

    add repo - irepo

---
+++ Dormitory Management/Domain/Abstractions/IRepository/IBilBillingRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Model;
+
+namespace Domain.Abstractions.IRepository;
+
+public interface IBilBillingRepository
+{
+    Task<IEnumerable<BilBilling>> Get();
+    Task<IEnumerable<BilBilling>> Search(
+        Guid? student);
+    void Create(BilBilling bilBilling);
+    void Update(BilBilling bilBilling);
+}
